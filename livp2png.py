4b5763fc81102d149bc96c5330e19ffa9b77fdb5
agente3z <agente3z@yahoo.com>
2023-05-22 16:56:03 +0200

    Added full linux support and a check to see if tar or unzip is installed

--- livp2png.py
+++ livp2png.py
@@ -1,5 +1,18 @@
 import os
 from heic_image_converter.Converter import Converter as converti
+from platform import system
+from subprocess import call
+
+if system() == 'Windows':
+    try:
+        call(['tar', '--version'], stdout=open(os.devnull, 'w'))
+    except FileNotFoundError:
+        raise Exception('tar not found, please install it')
+else:
+    try:
+        call(['unzip', '-v'], stdout=open(os.devnull, 'w'))
+    except FileNotFoundError:
+        raise Exception('unzip not found, please install it')
 
 listalivp=os.listdir()
 print(listalivp)
@@ -9,7 +22,7 @@ for e in listalivp:
     elif '.' not in e:
         pass
     elif '.livp' in e:
-        os.system(f'tar -xf "{e}"')
+        os.system(f'tar -xf "{e}"') if system() == 'Windows' else os.system(f'unzip -q "{e}"')
         #os.system(f'del "{e}"') 
         # remove the "#" on the previous line if you wish to delete the livp file in the current folder after unpacking it
     else:
@@ -21,7 +34,7 @@ ImgConverter.convert_files()
 for e in listaspacchettati:
     if '.mov' in e or ".heic" in e:
         pass
-        os.system(f'del "{e}"')
+        os.system(f'del "{e}"') if system() == 'Windows' else os.system(f'rm "{e}"')
     elif '.py' in e:
         pass
     elif '.' not in e:
